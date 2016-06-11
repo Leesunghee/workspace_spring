ec293974761fcb7a0095dbf46bbedeeaacc9b96c
sungheelee <realwunz@gmail.com>
2016-06-11 22:37:37 +0900

    add Assembler, MainForAssembler

---
+++ src/main/java/chap03/Assembler.java
@@ -0,0 +1,28 @@
+package chap03;
+
+/**
+ * Created by sungheelee on 2016. 6. 11..
+ */
+public class Assembler {
+    private MemberDao memberDao;
+    private MemberRegisterService regSvc;
+    private ChangePasswordService pwdSvc;
+
+    public Assembler() {
+        memberDao = new MemberDao();
+        regSvc = new MemberRegisterService(memberDao);
+        pwdSvc = new ChangePasswordService(memberDao);
+    }
+
+    public MemberDao getMemberDao() {
+        return memberDao;
+    }
+
+    public MemberRegisterService getMemberRegisterService() {
+        return regSvc;
+    }
+
+    public ChangePasswordService getChangePasswordService() {
+        return pwdSvc;
+    }
+}
