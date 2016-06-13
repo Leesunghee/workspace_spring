29a124929b1a27565c42796368c23cfabec016be
sungheelee <realwunz@gmail.com>
2016-06-13 20:15:42 +0900

    add VersionPriter

---
+++ src/main/java/chap03/versionPrinter.java
@@ -0,0 +1,21 @@
+package chap03;
+
+/**
+ * Created by sungheelee on 2016. 6. 12..
+ */
+public class VersionPrinter {
+    private int majorVersion;
+    private int minorVersion;
+
+    public void setMajorVersion(int majorVersion) {
+        this.majorVersion = majorVersion;
+    }
+
+    public void setMinorVersion(int minorVersion) {
+        this.minorVersion = minorVersion;
+    }
+
+    public void print() {
+        System.out.printf("이 프로그램의 버전은 %d.%d입니다\n\n", majorVersion, minorVersion);
+    }
+}
